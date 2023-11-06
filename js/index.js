82482aab93d3761d274493770f4c5def5f2d3c84
Braian-MancioniBoggon <104107377+Braian-MancioniBoggon@users.noreply.github.com>
2023-11-06 08:31:58 -0300

    comentarios agregados

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Declaro los elementos que necesito usar del HTML
 const numero1 = document.getElementById("primerNumero");
 const numero2 = document.getElementById("segundoNumero");
 const suma = document.getElementById("suma");
@@ -6,30 +7,36 @@ const multiplicacion = document.getElementById("multiplicacion");
 const division = document.getElementById("division");
 const resultado = document.getElementById("resultado");
 
+//Sumo los numeros ingresados y llamo a la funcion para mostrar el resultado
 function sumar (a,b,callback){
     let c = a + b;
     callback(c);
 }
 
+//Resto los numeros ingresados y llamo a la funcion para mostrar el resultado
 function restar (a,b,callback){
     let c = a - b;
     callback(c);
 }
 
+//Multiplico los numeros ingresados y llamo a la funcion para mostrar el resultado
 function multiplicar (a,b,callback){
     let c = a * b;
     callback(c);
 }
 
+//Divido los numeros ingresados y llamo a la funcion para mostrar el resultado
 function dividir (a,b,callback){
     let c = a / b;
     callback(c);
 }
 
+//Muestro el resultado
 function mostrar (c){
     resultado.value = c;
 }
 
+//Creo los eventos para los botones de sumar, restar, multiplicar y dividir
 suma.addEventListener("click", () => {sumar(Number(primerNumero.value),Number(segundoNumero.value),mostrar)});
 resta.addEventListener("click", () => {restar(Number(primerNumero.value),Number(segundoNumero.value),mostrar)});
 multiplicacion.addEventListener("click", () => {multiplicar(Number(primerNumero.value),Number(segundoNumero.value),mostrar)});
